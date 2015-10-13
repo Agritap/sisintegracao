d094631d48a939559584eab1036644343e6be368
unknown <rafaella@sonner.com.br>
2015-10-13 18:05:57 -0300

    alterei controle de mortalidade

--- agritap-web/src/main/java/com/agritap/sisintegracao/client/ui/ViewFactory.java
+++ agritap-web/src/main/java/com/agritap/sisintegracao/client/ui/ViewFactory.java
@@ -2,6 +2,7 @@ package com.agritap.sisintegracao.client.ui;
 
 import com.agritap.sisintegracao.client.ViewEnum;
 import com.agritap.sisintegracao.client.ui.configuracao.ConfiguracaoLote;
+import com.agritap.sisintegracao.client.ui.configuracao.ControleMortalidade;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Composite;
 
@@ -55,6 +56,12 @@ public class ViewFactory {
 			mw.addConteudo(new ConfiguracaoLote());
 			return mw;
 		}
+		MainWindow mwcm = getMainWindow ();
+		if(view.equals(ViewEnum.CONTROLE_MORTALIDADE)){
+			mwcm.addConteudo(new ControleMortalidade());
+			return mwcm;
+		}
+		
 		return null;
 	}
 
