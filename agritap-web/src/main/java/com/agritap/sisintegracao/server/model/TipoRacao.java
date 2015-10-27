07d63493b542a0f58b53353f0358ae6b54e2e660
thiago silva horacio dos santos <thiago.silvahds@gmail.com>
2015-10-27 10:55:18 -0200

    Adiciona tipo de ração

---
+++ agritap-web/src/main/java/com/agritap/sisintegracao/server/model/TipoRacao.java
@@ -0,0 +1,81 @@
+package com.agritap.sisintegracao.server.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@NamedQuery(name="tipoRacao.todos",query="select t from TipoRacao t")
+public class TipoRacao {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	private Boolean medicada;
+	
+	private Integer carencia;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataInicio;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataFim;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getMedicada() {
+		return medicada;
+	}
+
+	public void setMedicada(Boolean medicada) {
+		this.medicada = medicada;
+	}
+
+	public Integer getCarencia() {
+		return carencia;
+	}
+
+	public void setCarencia(Integer carencia) {
+		this.carencia = carencia;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+}
---
+++ agritap-web/src/main/java/com/agritap/sisintegracao/server/rest/RacoesServiceImpl.java
@@ -0,0 +1,37 @@
+package com.agritap.sisintegracao.server.rest;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.agritap.sisintegracao.server.model.Produtor;
+import com.agritap.sisintegracao.server.model.TipoRacao;
+import com.agritap.sisintegracao.server.to.ListAdapter;
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+
+@Path("/racoes")
+@Produces(MediaType.APPLICATION_JSON)
+public class RacoesServiceImpl {
+
+	@Inject
+	transient EntityManager em;
+
+	@GET
+	@Path("/tipo/todos")
+	public ListAdapter<TipoRacao> tipoRacaoTodos() {
+		List<TipoRacao>  resultado =  em.createNamedQuery("tipoRacao.todos", TipoRacao.class).getResultList(); 
+		ListAdapter<TipoRacao> r = new ListAdapter<TipoRacao>(resultado);
+		return r;
+		
+		
+	}
+
+}
